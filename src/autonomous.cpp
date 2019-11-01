83ec42b17b1cb494a7ba54dd1f35e4a1b64be3ad
Richard Liu <richard.y.liu@gmail.com>
2019-11-01 01:40:34 -0700

    Clean up profile movement code.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+using namespace std;
 using namespace pros;
 
 // Information about our motors, wheels, field and our robot:
@@ -18,6 +19,8 @@ using namespace pros;
 #define TICK_PER_INCH 72
 #define LOW_MARK (95 / 100)
 #define HIGH_MARK (105 / 100)
+#define DEFAULT_TIMEOUT 1000
+#define VERBOSE false
 
 
 extern int autonNumber;
@@ -66,19 +69,39 @@ void a_tare_position(){
 }
 
 /**
- * Reset the counting for all four drive motors.
+ * Profile move function.
  * See more at https://pros.cs.purdue.edu/v5/api/cpp/motors.html#tare-position
  */
-void a_move_relative(double RF_target, double RR_target, double LF_target, double LR_target, std::int32_t velocity){
-  left_drive.move_relative(LF_target, velocity);
-  left_rear_drive.move_relative(LR_target, velocity);
-  right_drive.move_relative(RF_target, velocity);
-  right_rear_drive.move_relative(RR_target, velocity);
+void a_profile_move(bool isAbsoluteMove, double RF_target, double RR_target,
+  double LF_target, double LR_target,std::int32_t velocity, int timeout){
+  if (VERBOSE) {
+    printf("Start moving. isAbsolute: %s, Timeout: %d, LF: %f, LR: %f, RF: %f, RR: %f\n",
+      isAbsoluteMove ? "Absolute" : "Relative", timeout,
+      left_drive.get_position(), left_rear_drive.get_position(),
+      right_drive.get_position(), right_rear_drive.get_position());
+  }
+
+  if (isAbsoluteMove) {
+    left_drive.move_absolute(LF_target, velocity);
+    left_rear_drive.move_absolute(LR_target, velocity);
+    right_drive.move_absolute(RF_target, velocity);
+    right_rear_drive.move_absolute(RR_target, velocity);
+  }
+  else {
+    left_drive.move_relative(LF_target, velocity);
+    left_rear_drive.move_relative(LR_target, velocity);
+    right_drive.move_relative(RF_target, velocity);
+    right_rear_drive.move_relative(RR_target, velocity);
+  }
 
   int counter = 0;
   while (true) {
-    //printf("Counter: %d, LF: %f, LR: %f, RF: %f, RR: %f\n", counter++, left_drive.get_position(), left_rear_drive.get_position(),
-    //  right_drive.get_position(), right_rear_drive.get_position());
+    counter++;
+    if (VERBOSE) {
+      printf("Counter: %d, LF: %f, LR: %f, RF: %f, RR: %f\n", counter,
+        left_drive.get_position(), left_rear_drive.get_position(),
+        right_drive.get_position(), right_rear_drive.get_position());
+    }
 
     double LF_pos = left_drive.get_position();
     double LR_pos = left_rear_drive.get_position();
@@ -91,72 +114,57 @@ void a_move_relative(double RF_target, double RR_target, double LF_target, doubl
         || !((abs(RR_pos) > abs(RR_target) * 95/100) &&  (abs(RR_pos) < abs(RR_target) * 105/100))
     ) {
       delay(20);
+      // Break the indefinite loop if the robot is blocked by something.
+      if ((timeout!=-1) && (counter * 20 > timeout)) break;
     }
     else {
-      printf("Finished moving. Counter: %d, LF: %f, LR: %f, RF: %f, RR: %f\n", counter, left_drive.get_position(), left_rear_drive.get_position(),
-        right_drive.get_position(), right_rear_drive.get_position());
       break;
     }
   }
-  printf("Finished moving.\n");
-  //while (!((left_drive.get_position() > LF_pos * LOW_MARK) &&  (left_drive.get_position() < LF_pos * HIGH_MARK))
-  //    || !((left_rear_drive.get_position() > LR_pos * LOW_MARK) &&  (left_rear_drive.get_position() < LR_pos * HIGH_MARK))
-  //    || !((right_drive.get_position() > RF_pos * LOW_MARK) &&  (right_drive.get_position() < RF_pos * HIGH_MARK))
-  //    || !((right_rear_drive.get_position() > RR_pos * LOW_MARK) &&  (right_rear_drive.get_position() < RR_pos * HIGH_MARK))
-  //  ) {
-  //  delay(2);
-  //}
-}
-
-void a_move_relative(double position, std::int32_t velocity){
-  printf("\nNEW relative!!! position = %f, low = %f, high=%f\n\n", position, position * 95/100, position * 105/100);
-
-  a_move_relative(position, position, position, position, velocity);
+  printf("Finished moving. Counter: %d, LF: %f, LR: %f, RF: %f, RR: %f\n", counter,
+    left_drive.get_position(), left_rear_drive.get_position(),
+    right_drive.get_position(), right_rear_drive.get_position());
 }
 
 /**
  * Reset the counting for all four drive motors.
  * See more at https://pros.cs.purdue.edu/v5/api/cpp/motors.html#tare-position
  */
-void a_move_absolute(double RF_pos, double RR_pos, double LF_pos, double LR_pos, std::int32_t velocity){
-  printf("Entering a_move_absolute(%f, %f, %f, %f)\n", LF_pos, LR_pos, RF_pos, RR_pos);
-  left_drive.move_absolute(LF_pos, velocity);
-  left_rear_drive.move_absolute(LR_pos, velocity);
-  right_drive.move_absolute(RF_pos, velocity);
-  right_rear_drive.move_absolute(RR_pos, velocity);
+void a_move_relative(double target, std::int32_t velocity){
+   a_profile_move(false, target, target, target, target, velocity, DEFAULT_TIMEOUT);;
+ }
 
-  int counter = 0;
+void a_move_relative(double target, std::int32_t velocity, int timeout){
+   a_profile_move(false, target, target, target, target, velocity, timeout);;
+ }
 
+ void a_move_relative(double RF_target, double RR_target, double LF_target, double LR_target,
+       std::int32_t velocity){
+   a_profile_move(false, RF_target, RR_target, LF_target, LR_target, velocity, DEFAULT_TIMEOUT);
+ }
 
-
-  while (true) {
-    //printf("Counter: %d, LF: %f, LR: %f, RF: %f, RR: %f\n", counter++, left_drive.get_position(), left_rear_drive.get_position(),
-    //  right_drive.get_position(), right_rear_drive.get_position());
-    if (!((left_drive.get_position() > LF_pos * 95/100) &&  (left_drive.get_position() < LF_pos * 105/100))
-        || !((left_rear_drive.get_position() > LR_pos * 95/100) &&  (left_rear_drive.get_position() < LR_pos * 105/100))
-        || !((right_drive.get_position() > RF_pos * 95/100) &&  (right_drive.get_position() < RF_pos * 105/100))
-        || !((right_rear_drive.get_position() > RR_pos * 95/100) &&  (right_rear_drive.get_position() < RR_pos * 105/100))
-    ) {
-      delay(20);
-    }
-    else {
-      printf("Finished moving. Counter: %d, LF: %f, LR: %f, RF: %f, RR: %f\n", counter, left_drive.get_position(), left_rear_drive.get_position(),
-        right_drive.get_position(), right_rear_drive.get_position());
-      break;
-    }
-  }
-  //while (!((left_drive.get_position() > LF_pos * LOW_MARK) &&  (left_drive.get_position() < LF_pos * HIGH_MARK))
-  //    || !((left_rear_drive.get_position() > LR_pos * LOW_MARK) &&  (left_rear_drive.get_position() < LR_pos * HIGH_MARK))
-  //    || !((right_drive.get_position() > RF_pos * LOW_MARK) &&  (right_drive.get_position() < RF_pos * HIGH_MARK))
-  //    || !((right_rear_drive.get_position() > RR_pos * LOW_MARK) &&  (right_rear_drive.get_position() < RR_pos * HIGH_MARK))
-  //  ) {
-  //  delay(2);
-  //}
+void a_move_relative(double RF_target, double RR_target, double LF_target, double LR_target,
+      std::int32_t velocity, int timeout){
+  a_profile_move(false, RF_target, RR_target, LF_target, LR_target, velocity, timeout);
 }
 
+
 void a_move_absolute(double position, std::int32_t velocity){
-  printf("\nNEW absolute!!! position = %f, low = %f, high=%f\n\n", position, position * 95/100, position * 105/100);
-  a_move_absolute(position, position, position, position, velocity);
+  a_profile_move(true, position, position, position, position, velocity, DEFAULT_TIMEOUT);
+}
+
+void a_move_absolute(double position, std::int32_t velocity, int timeout){
+  a_profile_move(true, position, position, position, position, velocity, timeout);
+}
+
+void a_move_absolute(double RF_target, double RR_target, double LF_target, double LR_target,
+    std::int32_t velocity){
+  a_profile_move(true, RF_target, RR_target, LF_target, LR_target, velocity, DEFAULT_TIMEOUT);
+}
+
+void a_move_absolute(double RF_target, double RR_target, double LF_target, double LR_target,
+    std::int32_t velocity, int timeout){
+  a_profile_move(true, RF_target, RR_target, LF_target, LR_target, velocity, timeout);
 }
 
 /**
@@ -340,13 +348,29 @@ void unfold() {
   delay(100);
 }
 
+void score_fancy() {
+  a_move_drive(0, 0);
+  a_move_intake(0);
+
+  hinge.set_encoder_units(E_MOTOR_ENCODER_ROTATIONS);
+  hinge.tare_position();
+  hinge.move_absolute(2, 50);
+  delay(2200);
+  a_move_hinge(0);
+
+  // Drive forward a little bit
+  a_tare_position();
+  double ticks = 1.2 * TICK_PER_INCH;
+  a_move_absolute(ticks, 20, 100);  // velocity 20%, timeout 100ms
+}
+
 /**
  * This takes advantage of V5's profiling movements and PID to control how far
  * the motors travel, instead of relying on time.
  */
 void small_fancy(bool isRed) {
+  // Start intake. Apply negative force on the arm to prevent it from undesired lifting.
   a_move_lift(-20);
-  // start in-take
   a_move_intake(127);
 
   // For direct drive, we will count ticks.
@@ -357,104 +381,126 @@ void small_fancy(bool isRed) {
   double ticks = 6 * TICK_PER_INCH;
   a_move_absolute(ticks, 75);
   printf("Done with first move. Timelap = %d\n", millis() - start_ts);
-  delay(450);
+  // pick up first cube
+  delay(400);
 
-  // 2. Drive forward 5.5" at lower speed, pick up 2nd Cube
+  // 2. Drive forward 5.5", pick up 2nd Cube
   ticks += 6.5 * TICK_PER_INCH;
-  printf("ticks = %f Before second move.\n", ticks);
-  a_move_absolute(ticks, 50);
+  a_move_absolute(ticks, 75);
   printf("Done with second move. Timelap = %d\n", millis() - start_ts);
-  pros::delay(650);
-  printf("after delay.\n");
+  pros::delay(600);
 
   // 3. Drive forward 5.5", pick up 3rd Cube
   ticks += 5.5 * TICK_PER_INCH;
-  printf("ticks = %f Before 3rd move.\n", ticks);
-  a_move_absolute(ticks, 50);
+  a_move_absolute(ticks, 75);
   printf("Done with 3rd move.Timelap = %d\n", millis() - start_ts);
   delay(650);
 
-  // 4. Drive forward 5.5", pick up 4th Cube
+  // 4. Drive forward 6", pick up 4th Cube
   ticks += 6 * TICK_PER_INCH;
   a_move_absolute(ticks, 50);
   printf("Done with 4th move.Timelap = %d\n", millis() - start_ts);
-  delay(700);
+  delay(800);
 
-  // 5. Drive forward 5.5", pick up 5th Cube
+  // 5. Drive forward 6", pick up 5th Cube
   ticks += 6 * TICK_PER_INCH;
   a_move_absolute(ticks, 50);
-  printf("Done with 5th move.Timelap = %d\n", millis() - start_ts);
   delay(450);
-  printf("\n****After 5th****\n");
-  printf("LF\tLR\tRF\tRR\n");
-  printf("%f\t%f\t%f\t%f", left_drive.get_position(), left_rear_drive.get_position(),
-    right_drive.get_position(), right_rear_drive.get_position());
-  printf("\n*********************\n");
+  printf("****After 5th cube\tRF: %f\tRR: %f\tLF: %f\tLR: %f Timelap: %d\n",
+    right_drive.get_position(), right_rear_drive.get_position(),
+    left_drive.get_position(), left_rear_drive.get_position(), millis() - start_ts);
 
   a_move_lift(0);
 
-  // 6. Backup 13.5" so that the center of robot is at the border of first tile and second tile
+  // 6. Backup to where the center of robot is at the border of first tile and second tile
   ticks -= 11.5 * TICK_PER_INCH;
   a_move_absolute(ticks, 75);
-  printf("Done with backing.Timelap = %d\n", millis() - start_ts);
   delay(100);
-  printf("Done with backing. Timelap = %d\n", millis() - start_ts);
-  printf("\n****After backing****\n");
-  printf("LF\tLR\tRF\tRR\n");
-  printf("%f\t%f\t%f\t%f", left_drive.get_position(), left_rear_drive.get_position(),
-    right_drive.get_position(), right_rear_drive.get_position());
-  printf("\n*********************\n");
+  printf("****After backing\tRF: %f\tRR: %f\tLF: %f\tLR: %f Timelap: %d\n",
+    right_drive.get_position(), right_rear_drive.get_position(),
+    left_drive.get_position(), left_rear_drive.get_position(), millis() - start_ts);
 
-  // 7. Turn 135 degrees, which maps to ~16" travel for each wheel.
-  a_tare_position();
-  ticks = 12.25 * TICK_PER_INCH;
+  // New 7
+  double temp_ticks = 12.25 * TICK_PER_INCH;
+  double RF_ticks;
+  double RR_ticks;
+  double LF_ticks;
+  double LR_ticks;
   if (isRed) {
     // Turn right. The right wheel travels backward. The left wheel travels forward
-    printf("Red alliance, turn right.\n");
-    a_move_relative(-1*ticks, -1*ticks, ticks, ticks, 75);
+    //printf("New!!! Red alliance, turn right.\n");
+    RF_ticks = ticks - temp_ticks;
+    RR_ticks = ticks - temp_ticks;
+    LF_ticks = ticks + temp_ticks;
+    LR_ticks = ticks + temp_ticks;
+    a_move_absolute(RF_ticks, RR_ticks, LF_ticks, LR_ticks, 75);
   }
   else {
-    printf("Blue alliance, turn left.\n");
-    a_move_relative(ticks, ticks, -1*ticks, -1*ticks, 75);
+    //printf("New!!! Blue alliance, turn left.\n");
+    RF_ticks = ticks + temp_ticks;
+    RR_ticks = ticks + temp_ticks;
+    LF_ticks = ticks - temp_ticks;
+    LR_ticks = ticks - temp_ticks;
+    a_move_absolute(RF_ticks, RR_ticks, LF_ticks, LR_ticks, 75);
   }
-  printf("Done with backing. Timelap = %d\n", millis() - start_ts);
-  printf("\n****After turn****\n");
-  printf("LF\tLR\tRF\tRR\n");
-  printf("%f\t%f\t%f\t%f", left_drive.get_position(), left_rear_drive.get_position(),
-    right_drive.get_position(), right_rear_drive.get_position());
-  printf("\n*********************\n");
+  delay(50);
+  printf("****After turning\tRF: %f\tRR: %f\tLF: %f\tLR: %f Timelap: %d\n",
+    right_drive.get_position(), right_rear_drive.get_position(),
+    left_drive.get_position(), left_rear_drive.get_position(), millis() - start_ts);
 
-  //delay(100);
+  // 8. Drive to the goal zone
+  temp_ticks = 20 * TICK_PER_INCH;
+  RF_ticks += temp_ticks;
+  RR_ticks += temp_ticks;
+  LF_ticks += temp_ticks;
+  LR_ticks += temp_ticks;
 
-  // 8. Drive to the goal zone.
-  a_tare_position();
-  ticks = 20 * TICK_PER_INCH;
-  a_move_relative(ticks, 100);
-  printf("Done with driving to the goal zone. Timelap = %d\n", millis() - start_ts);
+  a_move_absolute(RF_ticks, RR_ticks, LF_ticks, LR_ticks, 75);
   delay(100);
-  printf("\n****Before Score****\n");
-  printf("LF\tLR\tRF\tRR\n");
-  printf("%f\t%f\t%f\t%f", left_drive.get_position(), left_rear_drive.get_position(),
-    right_drive.get_position(), right_rear_drive.get_position());
-  printf("\n*********************\n");
+  printf("****Before adjust\tRF: %f\tRR: %f\tLF: %f\tLR: %f Timelap: %d\n",
+    right_drive.get_position(), right_rear_drive.get_position(),
+    left_drive.get_position(), left_rear_drive.get_position(), millis() - start_ts);
 
-  // 9. score
-  score();
-  printf("Done with scoring. Timelap = %d\n", millis() - start_ts);
 
-  // 10. Drive backward for 5"
+  // 9 Fine adjustment to robot position
+  if (isRed) {
+    right_drive.move_absolute(1900, 75);
+    right_rear_drive.move_absolute(1900, 75);
+    left_drive.move_absolute(3650, 75);
+    left_rear_drive.move_absolute(3650, 75);
+    delay(100);
+  }
+  else {
+    right_drive.move_absolute(3650, 75);
+    right_rear_drive.move_absolute(3650, 75);
+    left_drive.move_absolute(1900, 75);
+    left_rear_drive.move_absolute(1900, 75);
+    delay(100);
+  }
+  printf("****After adjust\tRF: %f\tRR: %f\tLF: %f\tLR: %f Timelap: %d\n",
+    right_drive.get_position(), right_rear_drive.get_position(),
+    left_drive.get_position(), left_rear_drive.get_position(), millis() - start_ts);
+
+
+  // 10. score
+  score_fancy();
+  printf("****After adjust\tRF: %f\tRR: %f\tLF: %f\tLR: %f Timelap: %d\n",
+    right_drive.get_position(), right_rear_drive.get_position(),
+    left_drive.get_position(), left_rear_drive.get_position(), millis() - start_ts);
+
+  // 11. Drive backward
   a_tare_position();
   ticks = -10 * TICK_PER_INCH;
   a_move_relative(ticks, 100);
-  printf("Done with backing up. Timelap = %d\n", millis() - start_ts);
+  printf("****All complete \tRF: %f\tRR: %f\tLF: %f\tLR: %f Timelap: %d\n",
+    right_drive.get_position(), right_rear_drive.get_position(),
+    left_drive.get_position(), left_rear_drive.get_position(), millis() - start_ts);
 
   // 11. Move back the hinge.
   a_move_hinge(-127);
   delay(400);
   a_move_hinge(0);
   printf("Done with moving back hinge. Timelap = %d\n", millis() - start_ts);
-  //hinge.move_relative((-1)*degrees_to_turn, 100);
-  //a_move_hinge(0);
 }
 
 void big_one_cube(bool isRed) {
@@ -617,36 +663,7 @@ void big2(bool isRed) {
   a_move_hinge(-127);
   delay(400);
   a_move_hinge(0);
-
-
 }
-
-/*
-void big2 (bool isRed) {
-  /// add: a_move_drive(100, 100)
-  /// add: delay(200);
-  /// add: a_move_drive(0, 0);
-  if (isRed) {
-    a_move_drive(-50, 50);
-  }
-  else {
-    a_move_drive(50, -50);
-  }
-  delay(1500);
-  a_move_drive(0, 0);
-  a_move_drive(-50, -50);
-  delay(2000);
-  a_move_drive(0, 0);
-  a_move_drive(50, 50);
-  delay(2000);
-  a_move_drive(0,0);
-  delay(1000);
-}
-*/
-/// ----------------------------------------------------------------------------
-
-
-
 
 void red_small()
 {
@@ -828,69 +845,12 @@ void red_skills() {
 */
 }
 
-
-/*
-void blue_big() {
-  // big(false);
-  // Sample Code
-  // Move forward
-  // a_move_drive(30,30);
-
-  // Move backward
-  // a_move_drive(-30, -30);
-
-  // Left turn
-  // a_move_drive(30, 0);
-
-  // right turn
-  // a_move_drive(0, 30);
-  a_move_intake(127);
-
-  // pick up first block
-  a_move_drive(127, 127);
-  delay(400);
-    // turn right at the location
-  a_move_drive(-50, 50);
-  delay(800);
-
-  // drive forward to pick up the second block
-  a_move_drive(80, 80);
-  delay(800);
-
-  a_move_drive(0,0);
-  delay(1000);
-
-  // hold the block
-  a_move_intake(20);
-  // turn right for 45 degrees
-  a_move_drive(-50, 50);
-
-  delay(650);
-
-  // drive to the goal zone
-  a_move_drive(50, 50);
-  delay(500);
-
-  a_move_intake(0);
-  a_move_drive(0, 0);
-
-  a_move_hinge(60);
-  delay(1800);
-
-  a_move_hinge(0);
-
-  a_move_drive(-50, -50);
-  delay(500);
-  a_move_drive(0, 0);
-}
-*/
-
 void one_cube(bool isRed) {
   // For direct drive, we will count ticks.
   a_set_drive_encoding(E_MOTOR_ENCODER_COUNTS);
   a_tare_position();
 
-  // 1. Drive forward 6", pick up first Cube
+  // 1. Drive backward to push the cube into goal zone
   double ticks = (-14) * TICK_PER_INCH;
   a_move_relative(ticks, 100);
   printf("Done with first move. \n");
@@ -903,18 +863,19 @@ void one_cube(bool isRed) {
 
 void autonomous() {
 
-  printf("Welcome to Auton: 10/30 8:28PM. Force to call Red Small.\n");
+  printf("Welcome to Auton: 11/1 12:00AM. \n");
 
-  // **** at game time, uncomment the one that we are going to use and ***
-  //redAlliance = false;
-  redAlliance = true;
+  // **** For testing only, hardcode test case.
+  // **** At game time, we will select from the menu
+  //redAlliance = true;
+  redAlliance = false;
 
   //autonNumber = 1;  // One cube program
   //autonNumber = 2;  // 2 cube program for large goal zone
   autonNumber = 3;  // 5 cube program for small goal zone
   //autonNumber = 4;  // skill test program
 
-  int start_ts = millis();
+  start_ts = millis();
   unfold();
   printf("Done with unfolding. Timelap = %d\n", millis() - start_ts);
 
@@ -948,21 +909,11 @@ void autonomous() {
         blue_big();
         break;
       case 3:
-        blue_small();
+        small_fancy(false);
         break;
       case 4:
         //TODO: Add blue skills
         break;
     }
   }
-
-
-
-
-  // red_small();
-  // red_big();
-  // blue_small();
-  // blue_big();
-  //red_skills();
-
 }
